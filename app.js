ab13e2d72a5225f2b65c58b0605a410b53d7e4f3
Shabanshaikhramajn <160201863+Shabanshaikhramajn@users.noreply.github.com>
2024-08-20 15:38:45 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,9 @@
+const parent = React.createElement("div", {id:"parent"},)
+
+
+
+
+const heading  = React.createElement("h1", {id:"heading", xyz :"abc"}, "hello world from react");
+
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(heading)
